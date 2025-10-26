330524632ec19154c44e7f1a9eca858814a70791
Kai <kaixiang980117@gmail.com>
2025-10-26 21:45:16 +0000

    es

---
+++ week3/week3q2.py
@@ -0,0 +1,1 @@
+def has_duplicates
--- week3q2.py
+++
@@ -1,7 +0,0 @@
-def has_duplicates(words):
-    seen = {}  # dictionary to store encountered words
-    for i in words:
-        if i in seen:
-            return True  # duplicate found
-        seen[i] = None  # store word as key
-    return False  # no duplicates found

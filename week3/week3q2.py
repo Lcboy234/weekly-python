5f0024a8376ee3032f47d2f28689bd6c1df3fe3f
Kai <kaixiang980117@gmail.com>
2025-10-26 22:01:21 +0000

    q2 done

--- week3/week3q2.py
+++ week3/week3q2.py
@@ -1,2 +1,13 @@
-def has_duplicates(a):
-    
+def has_duplicates(input):
+    seen = {}
+
+    for word in input:
+        if word in seen:
+            return True
+        seen[word] = None
+    return False
+
+first = has_duplicates(['the', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'])
+print(first)
+second = has_duplicates(['magic', 'tree', 'house'])
+print(second)
